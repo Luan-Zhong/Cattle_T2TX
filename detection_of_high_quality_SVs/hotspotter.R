14f8ec3d2e70128dac68da3f679e1e39fcd603a2
Callum MacPhillamy <65528813+cmacphillamy@users.noreply.github.com>
2025-06-26 11:06:20 +0930

    chore: Increase comment verbosity

--- detection_of_high_quality_SVs/hotspotter.R
+++ detection_of_high_quality_SVs/hotspotter.R
@@ -5,8 +5,15 @@ library(karyoploteR)
 # This script is an example of what was used to identify hotspots. Paths have
 # been change for demonstration purposes. Parameters remain the same.
 
-seqlens <- read.csv('/Users/callummacphillamy/Projects/REFERENCES/UOA_WAGYU/UOA_Wagyu_1.withY.fa.fai',
+# Update paths and uncomment the below lines to run on your machine
+# CHROM_SIZES <- "/path/to/UOA_Wagyu_1.withY.fa.fai"
+# SV_BED <- "/path/to/SV.bed" # Bed file of SVs from your caller(s)
+# OUT_HOTSPOT_TSV <- "/path/to/desired/save/location/hotspot.tsv"
+# OUT_HOTSPOT_BED <- "/path/to/desired/save/location/hotspot.bed"
+
+seqlens <- read.csv(CHROM_SIZES,
                     header=FALSE, sep='\t') 
+
 # Insert column of 1s for the start position
 seqlens$start <- 1
 
@@ -18,21 +25,22 @@ seqlens <- seqlens[!grepl('MT', seqlens$V1),]
 
 seqinfo <- Seqinfo(seqnames=seqlens$V1, seqlengths=seqlens$V2)
 
-
-SV_BED <- "/path/to/SV.bed"
-
+# Load the SV bed file
 x <- read.table(SV_BED,
 col.names=c('chrom','start','end','name','score','strand'), header=FALSE, sep='\t')
 
+# Make a GRanges object from the SV bed file dataframe
 x_gr <- makeGRangesFromDataFrame(x, seqinfo = seqinfo)
 x_gr <- trim(x_gr, use.names=TRUE)
+
+# Run hotspotter
 res <- hotspotter(x_gr, bw=200000, num.trial = 2000)
 
 res_df <- data.frame(res)
-write.table(res_df, file="/path/to/SV_hotspots.tsv")
+write.table(res_df, file=OUT_HOTSPOT_TSV)
 
 res_bed <- res_df[, c('seqnames','start','end','num.events','pvalue','strand')]
-write.table(res_bed, file="/path/to/SV_hotspots.bed",
+write.table(res_bed, file=OUT_HOTSPOT_BED,
             sep='\t',
             col.names=FALSE,
             row.names=FALSE,
